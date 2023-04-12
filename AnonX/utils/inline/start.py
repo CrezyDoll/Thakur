babe13bbc3e9ac8fe72e479ef5083f51bdf23629
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-04-12 21:44:35 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -44,7 +44,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text="🌹 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🌹", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🌹 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🌹", url=f"https://t.me/WCFnetwork",
+                text="🌹 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🌹", url=f"https://t.me/IIHST_NTWRK",
             )
         ],
         [
